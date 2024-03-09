2591aaa8e413f6638d8a8012eace6963d16789e4
moktarul12 <moktarul@gmail.com>
2024-03-09 11:43:25 +0530

    release

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("test----");
+});
+
+
 app.get("/api/test", (req, res) => {
   res.send("test");
 });
